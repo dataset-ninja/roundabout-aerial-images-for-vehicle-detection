4278a56aadef09128e4f4d7b6a7441690dfb6cd0
Ars Vladimirov <grokhael@gmail.com>
2023-08-21 15:40:42 +0600

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Roundabout Aerial Images"
 PROJECT_NAME_FULL: str = "Roundabout Aerial Images for Vehicle Detection"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -94,6 +95,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
